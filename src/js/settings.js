6ac9e985e3a4031ef8c8ccacd1c0c64df597c1d6
Hanna Boratynska <boratynskahanna@gmail.com>
2021-05-06 10:56:03 +0200

    Correct files

--- src/js/settings.js
+++ src/js/settings.js
@@ -123,7 +123,7 @@ export const settings = {
     defaultDeliveryFee: 20,
   },
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     product: 'product',
     order: 'order',
     booking: 'booking',
